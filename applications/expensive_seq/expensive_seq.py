f1cbd54dbe6e8ef7b56e462397a0bb0330331d40
robomantis19 <freefavors@gmail.com>
2020-07-15 21:12:25 -0400

    finished assignment

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -2,7 +2,8 @@
 
 
 def expensive_seq(x, y, z):
-    # Your code here
+     if x <= 0: y + z
+     if x >  0: expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
 
 
 
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,20 @@
 # Your code here
 
+file = open('robin.txt', 'r')
+string = []
+dictStart={}
+for line in file:  
+    value = ', '.join(line.split(" "))
+    string.append(value)
+        
+
+words = string 
+# .split(" \'")
+words
+
+print(words)
+# count = 0
+# for i in words: 
+#     if count == 0: 
+#         print(i)
+#     count += 1
